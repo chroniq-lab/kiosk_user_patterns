21ec99d2c66b0719181b1e770485704f60890fb5
cchizak <cchizak@emory.edu>
2025-06-20 23:35:13 -0400

    Updated kupdat02

--- config.py
+++ config.py
@@ -11,4 +11,6 @@ if USER == "JVARGH7":
     PATH_KIOSK_USER_PATTERNS_FOLDER = Path("C:/Cloud/OneDrive - Emory University/Papers/Pursuant User Profiles")
     PATH_KIOSK_USER_PATTERNS_REPO = Path("C:/code/external/kiosk_user_patterns")
     # test
-
+if USER == "carolinechizak":  # Replace with your actual username
+    PATH_KIOSK_USER_PATTERNS_FOLDER = Path("/Users/carolinechizak/Library/CloudStorage/OneDrive-SharedLibraries-Emory/Varghese, Jithin Sam - Pursuant User Profiles")  # Set your path here
+    PATH_KIOSK_USER_PATTERNS_REPO = Path("/Users/carolinechizak/kiosk_user_patterns")    # Set your path here
