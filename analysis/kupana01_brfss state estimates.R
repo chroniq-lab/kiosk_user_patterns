589a2a83329254debd07fb58bc27d4bc11f34464
jvargh7 <jvargh7@emory.edu>
2025-07-21 11:10:07 -0400

    Commit with example BRFSS state estimation in R kupana01

---
+++ analysis/kupana01_brfss state estimates.R
@@ -0,0 +1,151 @@
+rm(list=ls());gc();source(".Rprofile")
+
+
+# "_RACE1": Race/ethnicity
+# "_RACEPR1": Race
+# "_HISPANC": Hispanic
+
+brfss2022 = open_dataset(paste0(path_brfss_parquet_folder, "/year=2022/"), format = "parquet") %>%
+  dplyr::select(state, year, one_of("_PSU", "_STSTR", "_LLCPWT",
+  "PREGNANT", "WEIGHT2", "HEIGHT3", "WTKG3", "HTM4", "_BMI5", "_BMI5CAT",
+  "_SEX", "_AGEG5YR", "_AGE65YR", "_AGE80", "_RACE1","_RACEPR1", "_HISPANC",
+  "MARITAL", "EDUCA", "INCOME3", "_HLTHPLN")) %>%
+  dplyr::rename(PSU = "_PSU",
+                STSTR = "_STSTR",
+                LLCPWT = "_LLCPWT")  |>
+    mutate(
+    state_names = case_when(
+      state == 1 ~ "Alabama",
+      state == 2 ~ "Alaska",
+      state == 4 ~ "Arizona",
+      state == 5 ~ "Arkansas",
+      state == 6 ~ "California",
+      state == 8 ~ "Colorado",
+      state == 9 ~ "Connecticut",
+      state == 10 ~ "Delaware",
+      state == 11 ~ "District of Columbia",
+      state == 12 ~ "Florida",
+      state == 13 ~ "Georgia",
+      state == 15 ~ "Hawaii",
+      state == 16 ~ "Idaho",
+      state == 17 ~ "Illinois",
+      state == 18 ~ "Indiana",
+      state == 19 ~ "Iowa",
+      state == 20 ~ "Kansas",
+      state == 21 ~ "Kentucky",
+      state == 22 ~ "Louisiana",
+      state == 23 ~ "Maine",
+      state == 24 ~ "Maryland",
+      state == 25 ~ "Massachusetts",
+      state == 26 ~ "Michigan",
+      state == 27 ~ "Minnesota",
+      state == 28 ~ "Mississippi",
+      state == 29 ~ "Missouri",
+      state == 30 ~ "Montana",
+      state == 31 ~ "Nebraska",
+      state == 32 ~ "Nevada",
+      state == 33 ~ "New Hampshire",
+      state == 34 ~ "New Jersey",
+      state == 35 ~ "New Mexico",
+      state == 36 ~ "New York",
+      state == 37 ~ "North Carolina",
+      state == 38 ~ "North Dakota",
+      state == 39 ~ "Ohio",
+      state == 40 ~ "Oklahoma",
+      state == 41 ~ "Oregon",
+      state == 42 ~ "Pennsylvania",
+      state == 44 ~ "Rhode Island",
+      state == 45 ~ "South Carolina",
+      state == 46 ~ "South Dakota",
+      state == 47 ~ "Tennessee",
+      state == 48 ~ "Texas",
+      state == 49 ~ "Utah",
+      state == 50 ~ "Vermont",
+      state == 51 ~ "Virginia",
+      state == 53 ~ "Washington",
+      state == 54 ~ "West Virginia",
+      state == 55 ~ "Wisconsin",
+      state == 56 ~ "Wyoming",
+      state == 66 ~ "Guam",
+      state == 72 ~ "Puerto Rico",
+      state == 78 ~ "Virgin Islands",
+      TRUE ~ NA_character_),
+
+    sex = case_when( 
+      `_SEX` == 1 ~ "Male",
+      `_SEX` == 2 ~ "Female"),
+    hispanic = case_when(
+        `_HISPANC` == 1 ~ "Hispanic",
+        `_HISPANC` == 2 ~ "Non-Hispanic",
+        `_HISPANC` == 9 ~ "Non-Hispanic", # Don't know/Refused/Missing
+      TRUE ~ NA_character_
+    ),
+    raceeth = case_when(
+      `_RACE1` == 1 ~ "NH white",
+      `_RACE1` == 2 ~ "NH black",
+      `_RACE1` %in% c(3,5,6,7,9) ~ "NH other", # 9: Don't know/Refused/Missing
+      `_RACE1` == 4 ~ "NH asian",
+      `_RACE1` == 8 ~ "Hispanic",
+      TRUE ~ NA_character_
+    ),
+    height_cm = HTM4/100,
+    weight_kg = WTKG3/100,
+    bmi_category = case_when(
+        `_BMI5CAT` == 1 ~ "Underweight",
+        `_BMI5CAT` == 2 ~ "Normal weight",
+        `_BMI5CAT` == 3 ~ "Overweight",
+        `_BMI5CAT` == 4 ~ "Obese",
+        `_BMI5CAT` == 5 ~ "Missing",
+        TRUE ~ NA_character_
+    ),
+
+    age_group = case_when(
+      `_AGEG5YR` %in% sprintf("%02d", 1:5) ~ "18-44",
+      `_AGEG5YR` %in% sprintf("%02d", 6:9) ~ "45-64", 
+      `_AGEG5YR` %in% sprintf("%02d", 10:13) ~ "65plus",
+      `_AGEG5YR` %in% 14 ~ NA_character_,
+      TRUE ~ NA_character_
+    )) %>%
+dplyr::filter(!is.na(age_group)) %>%
+dplyr::filter(!is.na(age_group)) %>% 
+dplyr::filter(sex == "Male" |(sex == "Female" & PREGNANT %in% c(2,7,9))) %>%
+dplyr::filter(state != 66 & state != 72 & state != 78) %>%
+collect()
+
+
+library(srvyr)
+options(survey.lonely.psu = "adjust")
+brfss2022_svy <- brfss2022 %>% 
+  as_survey_design(.data = .,
+                   ids = PSU,
+                   strata = STSTR,
+                   weights = LLCPWT, 
+                   nest = TRUE, 
+                   ps = "brewer",variance = "YG")
+
+
+# Method 2 using srvyr
+state_estimates_m2 = brfss2022_svy %>%
+  group_by(state, state_names, bmi_category) %>%
+  summarise(
+    n = survey_total(),
+    unwt_freq = unweighted(n()),
+    prop = survey_prop(vartype="ci"),
+    .groups = "drop") %>%
+  ungroup()
+View(state_estimates_m2)
+
+
+# Method 3 of https://github.com/chroniq-lab/tutorials/blob/main/NHANES/tutnha03_descriptive%20characteristics.R
+# source("C:/code/external/functions/survey/svysummary.R")
+
+source("https://raw.githubusercontent.com/jvargh7/functions/main/survey/svysummary.R")
+
+
+state_estimates_m3 = svysummary(
+  brfss2022_svy,
+  g_vars=c("age_group","bmi_category"),
+  id_vars=c("state","state_names"))
+
+View(state_estimates_m3)
+write_csv(state_estimates_m3,"analysis/kupana01_brfss state estimates.csv")
